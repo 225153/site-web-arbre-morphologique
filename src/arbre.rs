3df38d6f92f5dec7cd0a2dd58da84e6f663a6eab
ahmedtr692 <ahmedtr692@gmail.com>
2026-01-24 13:27:22 +0100

    .

--- src/arbre.rs
+++ src/arbre.rs
@@ -11,7 +11,7 @@ pub struct RacineNode {
 }
 
 
-pub fn morphologic_cmp(tree_racine : [char;3], racine : [char; 3])-> i8 {
+pub fn morphologic_cmp(tree_racine : [char;3], racine : [char;3])-> i8 {
     let mut i = 0 ; 
     while i < 3 {
         if tree_racine[i] == racine[i] {
@@ -30,19 +30,53 @@ pub fn morphologic_cmp(tree_racine : [char;3], racine : [char; 3])-> i8 {
 }
 
 
-pub struct Tree{Option<Box<RacineNode>>};
+pub struct Tree{
+    pub racine : Option<Box<RacineNode>> ,
+};
+
+
 impl RacineNode{
     pub fn new(racine: [char; 3]) -> Self {
         RacineNode{
             racine,
-            left: none ,
-            right : none,
+            left: None ,
+            right : None,
         }
+    }
+    pub fn verify_node(&self, ch : [char;3])-> bool {
+        let cmp = morphologic_cmp(self.racine, ch) ; 
+        if cmp == 0 {return true ;}
+        else {
+            if cmp == -1 {
+                if self.left == None {
+                    return false ;
+                }
+                else {
+                    return self.left.as_ref().unwrap().verify_node(ch);
+                }
+            }
+            else {
+                if self.right == None {
+                    return false ;
+                }
+                else{
+                    return self.right.as_ref().unwrap().verify_node(ch)
+                }
+            }
+        }
+
     }
 }
 impl Tree {
-    pub fn new() -> Self {
+    pub fn new() -> Self { 
         Tree { racine: None }
     }
-    pub fn verify()
+    pub fn verify(&self, ch : [char;3])-> bool {
+        if self.racine.is_none() {
+            return false ;
+        }
+        let node = self.racine.as_ref().unwrap() ; 
+        return node.verify_node(ch) ; 
+
+    }
 }
